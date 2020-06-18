08675833926c9303ad5ce800cb38ab484474f060
ntpaul <ntpaul@clemson.edu>
2020-06-18 15:36:21 -0400

    Working Firebase connection and video

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import './App.css';
 import VideoChat from './VideoChat';
+import firebase from './Firebase.js';
+
+
+
 
 const App = () => {
+  //firebase code read
+  const itemsRef = firebase.database().ref('roomIDs');
+  itemsRef.on('value', (snapshot) => {
+    let items = snapshot.val();
+    let newState = [];
+    for (let item in items) {
+      newState.push({
+        roomid: item,
+        users: items[item].users,
+      });
+    }
+    console.log(newState);
+  });
+
+  ///
+
+  
   return (
     <div className="app">
       <header>
