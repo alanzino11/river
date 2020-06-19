ea38068ccd6258d88dafa13857125a0839ee8119
alanzino11 <annalanzino@ufl.edu>
2020-06-19 14:52:29 -0400

    added form

---
+++ frontend/src/formData.js
@@ -0,0 +1,34 @@
+let interestOptions = [
+    { value: 'music', label: 'Music' },
+    { value: 'technology', label: 'Technology' },
+    { value: 'sports', label: 'Sports' },
+    { value: 'board games', label: 'Board Games' },
+    { value: 'reading', label: 'Reading' },
+    { value: 'gardening', label: 'Gardening' },
+    { value: 'cooking', label: 'Cooking' },
+    { value: 'animals', label: 'Animals' },
+    { value: 'fitness', label: 'Fitness' },
+    { value: 'photography', label: 'Photography' },
+    { value: 'decorating', label: 'Decorating' },
+    { value: 'travelling', label: 'Travelling' },
+    { value: 'baking', label: 'Baking' },
+];
+
+let departments = [
+    { value: 'digital marketing', label: 'Digital Marketing' },
+    { value: 'business leadership', label: 'Business Leadership' },
+    { value: 'creative', label: 'Creative' },
+    { value: 'data & analytics', label: 'Data & Analytics' },
+    { value: 'tech', label: 'Tech' },
+    { value: 'sales', label: 'Sales' },
+];
+
+let yearsWorkedOptions = [
+    { value: 'Intern', label: 'Intern' },
+    { value: '0 - 1 year', label: '< 1 year' },
+    { value: '1 - 5 years', label: '1 - 5 years' },
+    { value: '5 - 10 years', label: '5 - 10 years' },
+    { value: '10+ years', label: '> 10 years' },
+];
+
+export {interestOptions, departments, yearsWorkedOptions}
