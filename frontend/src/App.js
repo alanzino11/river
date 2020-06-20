e1181350cb4208b281a9dd8f4d936a2ab5c43930
ntpaul <ntpaul@clemson.edu>
2020-06-20 15:32:08 -0400

    firstpush

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,16 @@
 import React from 'react';
 import './App.css';
 import VideoChat from './VideoChat';
-import firebase from './Firebase.js';
-
-
 
 
 const App = () => {
-  //firebase code read
-  const itemsRef = firebase.database().ref('roomIDs');
-  itemsRef.on('value', (snapshot) => {
-    let items = snapshot.val();
-    let newState = [];
-    for (let item in items) {
-      newState.push({
-        roomid: item,
-        users: items[item].users,
-      });
-    }
-    console.log(newState);
-  });
 
-  ///
-
-  
   return (
     <div className="app">
       <header>
         <h1>Video Chat</h1>
       </header>
-      <main>
+      <main> 
         <VideoChat />
       </main>
       
--- frontend/src/Lobby2.js
+++ frontend/src/Lobby2.js
@@ -7,41 +7,47 @@ by using the functions passed in from VideoChat*/
 import firebase from './Firebase.js';
 
 
+var rooms = [];
 
+export default class Lobby2 extends Component{
 
-const Lobby = ({
-  username,
-  handleUsernameChange,
-  roomName,
-  handleRoomNameChange,
-  handleSubmit
-}) => {
+  state = {
+      newState : [],
+      items : null,
+  }
 
-  const newState = [];
-
+  componentDidMount(){
   const itemsRef = firebase.database().ref('roomIDs');
   itemsRef.on('value', (snapshot) => {
-    let items = snapshot.val();
-    for (let item in items) {    
-      newState.push({
-      roomid: item,
-      users: items[item].users,
-      });
-    }
-    console.log(newState[0].roomid);
-    console.log(newState);
+   this.state.items = snapshot.val();
+   this.setItems();
   });
+   
+  }
 
+  setItems = () => {
+    for (let item in this.state.items) {    
+      this.state.newState.push({
+          roomid: item,
+          users: this.state.items[item].users,
+          });
+      
+  }
+  console.log(this.state.newState[0].roomid);
+  console.log(this.state.newState);
+  }
+
+  render(){
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={this.props.handleSubmit}>
       <h2>Enter a room</h2>
       <div>
         <label htmlFor="name">Name:</label>
         <input
           type="text"
           id="field"
-          value={username}
-          onChange={handleUsernameChange}
+          value={this.props.username}
+          onChange={this.props.handleUsernameChange}
           required
         />
       </div>
@@ -51,14 +57,14 @@ const Lobby = ({
         <input
           type="text"
           id="room"
-          value={roomName}
-          onChange={handleRoomNameChange}
+          value={this.props.roomName}
+          onChange={this.props.handleRoomNameChange}
           required
         />
 
     <select>
   	  <option value="" selected>Select Your Stream</option>
-      <option value="saab">Saab</option>
+      <option value="saab">saab</option>
       <option value="fiat">Fiat</option>
       <option value="audi">Audi</option>
     </select>
@@ -67,6 +73,5 @@ const Lobby = ({
       <button type="submit">Submit</button>
     </form>
   );
+  }
 };
-
-export default Lobby;
