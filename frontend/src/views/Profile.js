0ab681c13b66cc3375ee91f017975472452f6b68
alanzino11 <annalanzino@ufl.edu>
2020-06-22 10:40:13 -0400

    interest form is editable via profile

--- frontend/src/views/Profile.js
+++ frontend/src/views/Profile.js
@@ -1,17 +1,48 @@
-import React from "react";
-import { Container, Row, Col } from "reactstrap";
+import React, {useEffect, useState} from "react";
+import { Container, Row, Col, Button } from "reactstrap";
+import Modal from 'react-modal'
 
+import firebase from '../firebase';
 import Loading from "../components/Loading";
 import { useAuth0 } from "../react-auth0-spa";
+import InterestForm from '../components/InterestForm';
 
 const Profile = () => {
   const { loading, user } = useAuth0();
+  const [interests, setInterests] = useState([])
+  const [interestFormOpen, setInterestFormOpen] = useState(false)
+
+  useEffect(() => {
+    const verifyUserRegistered = () => {
+      firebase.database().ref("users/"+ user.nickname)
+      .on('value', (snapshot) => {
+        let userObj = snapshot.val();
+        splitArray(userObj.interests.topics)
+      });
+    }
+
+    verifyUserRegistered()
+  }, [])
+
+  const splitArray = (str) => {
+    let fields = str.split(',');
+    setInterests(fields)
+  }
 
   if (loading || !user) {
     return <Loading />;
   }
 
   return (
+    <>
+    <Modal
+      isOpen={interestFormOpen}
+      onRequestClose={() => setInterestFormOpen(false)}
+      contentLabel="Example Modal"
+      ariaHideApp={false}
+    >
+      <InterestForm closeModal={() => setInterestFormOpen(false)}/>
+    </Modal>
     <Container className="mb-5">
       <Row className="align-items-center profile-header mb-5 text-center text-md-left">
         <Col md={2}>
@@ -24,12 +55,12 @@ const Profile = () => {
         <Col md>
           <h2>{user.name}</h2>
           <p className="lead text-muted">{user.email}</p>
+          <p className="lead text-muted">Interests: {interests}</p>
+          <Button onClick={() => setInterestFormOpen(true)}>Edit Interest Form</Button>
         </Col>
       </Row>
-      <Row>
-        {JSON.stringify(user, null, 2)}
-      </Row>
     </Container>
+    </>
   );
 };
 
